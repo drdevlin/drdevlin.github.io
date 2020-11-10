82b15f25006cbd1dc4aeeedb3c69f48952f1ec57
drdevlin <drdevlin@fastmail.com>
2020-11-10 11:26:01 -0500

    Tweak lightness of colorblocks

--- scripts/index.js
+++ scripts/index.js
@@ -25,8 +25,26 @@ const colorBlocks = [];
 colorBlocksNL.forEach(node => colorBlocks.push(node)); //Push all colorblocks into an array
 const colorBlocksShuffled = shuffle(colorBlocks); //Shuffle the colorblocks array
 colorBlocksShuffled.forEach((block, i) => {
-  block.style.backgroundColor = 'hsl(215, 70%, ' + (i + 18) * 2 + '%)'; //Color, but increment the lightness each time
+  block.style.backgroundColor = 'hsl(215, 70%, ' + (i + 25) * 1.7 + '%)'; //Color, but increment the lightness each time
 });
 
+/* Disabling this for now
+/* Show clickable elements when attempting to scroll
+const contact = document.querySelector('nav.contact');
+const skills = document.querySelector('nav.skills');
+const weather = document.querySelector('nav.weather img');
 
+function flashButtons() {
+  contact.style.opacity = '0.9';
+  skills.style.opacity = '0.9';
+  weather.style.width = '380px';
+  setTimeout(() => {
+    contact.style.opacity = 'initial';
+    skills.style.opacity = 'initial';
+    weather.style.width = '400px';
+    document.removeEventListener('scroll', flashButtons);
+  }, 250);
+}
 
+document.addEventListener('scroll', flashButtons);
+*/
