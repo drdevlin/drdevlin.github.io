35d2ab5af81f6679fdea845fdc42eba8e32f1ca3
drdevlin <drdevlin@fastmail.com>
2024-05-30 12:42:17 -0400

    add background gradient animation

---
+++ init.js
@@ -0,0 +1,24 @@
+CSS.registerProperty({
+  name: '--x1',
+  syntax: '<length-percentage>',
+  inherits: true,
+  initialValue: '10%',
+});
+CSS.registerProperty({
+  name: '--y1',
+  syntax: '<length-percentage>',
+  inherits: true,
+  initialValue: '10%',
+});
+CSS.registerProperty({
+  name: '--x2',
+  syntax: '<length-percentage>',
+  inherits: true,
+  initialValue: '90%',
+});
+CSS.registerProperty({
+  name: '--y2',
+  syntax: '<length-percentage>',
+  inherits: true,
+  initialValue: '90%',
+});
